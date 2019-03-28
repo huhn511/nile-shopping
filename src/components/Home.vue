8f490da41365c5046c4b4cd103bdab72b8eb3edf
huhn <sehe89@gmail.com>
2019-03-28 10:02:35 +0100

    add number icons

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -23,15 +23,30 @@
           <el-col :span="12">
             <h3 class="section-hero">Features</h3>
             <el-collapse v-model="activeNames" @change="handleChange">
-              <el-collapse-item title="Discover local shops" name="1">
+              <el-collapse-item name="1">
+                <template slot="title">
+                  <span>
+                    <span class="counter">1</span>Discover local shops
+                  </span>
+                </template>
                 <div>Consistent with real life: in line with the process and logic of real life, and comply with languages and habits that the users are used to;</div>
                 <div>Consistent within interface: all elements should be consistent, such as: design style, icons and texts, position of elements, etc.</div>
               </el-collapse-item>
-              <el-collapse-item title="Buy online with same day delivery" name="2">
+              <el-collapse-item name="2">
+                <template slot="title">
+                  <span>
+                    <span class="counter">2</span>Buy online with same day delivery
+                  </span>
+                </template>
                 <div>Operation feedback: enable the users to clearly perceive their operations by style updates and interactive effects;</div>
                 <div>Visual feedback: reflect current state by updating or rearranging elements of the page.</div>
               </el-collapse-item>
-              <el-collapse-item title="Privacy by design" name="3">
+              <el-collapse-item name="3">
+                <template slot="title">
+                  <span>
+                    <span class="counter">3</span> Privacy by design
+                  </span>
+                </template>
                 <div>Simplify the process: keep operating process simple and intuitive;</div>
                 <div>Definite and clear: enunciate your intentions clearly so that the users can quickly understand and make decisions;</div>
                 <div>Easy to identify: the interface should be straightforward, which helps the users to identify and frees them from memorizing and recalling.</div>
