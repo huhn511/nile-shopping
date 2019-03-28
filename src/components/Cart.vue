90151a0512dc481c6ee182fb033e7475bd68e912
huhn <sehe89@gmail.com>
2019-03-28 03:53:45 +0100

    add order request function

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="cart">
     <h1 class="title">Your Shopping Cart</h1>
+    <p>For shop: {{shop.data.name}}</p>
     <p v-show="!products.length">
       <i>Your cart is empty!</i>
       <router-link to="/products">Go shopping</router-link>
@@ -21,7 +22,14 @@
 
 <script>
 import { mapGetters } from "vuex";
+import { provider } from '@/config.json';
+import { composeAPI } from '@iota/core'
+import { generateSeed } from '../utlls/MAM'
+const Converter = require('@iota/converter')
 
+const iota = composeAPI({
+    provider: provider
+})
 export default {
   computed: {
     ...mapGetters({
@@ -31,11 +39,80 @@ export default {
       return this.products.reduce((total, p) => {
         return total + p.price * p.quantity;
       }, 0);
+    },
+    shop () {
+      return this.$store.state.shop
     }
   },
   methods: {
     checkout() {
-      alert("Pay us " + this.total + " €");
+      this.$confirm('If you press OK, a request will be sent to create an order which costs ' + this.total + '€. Continue?', 'Order request!', {
+          confirmButtonText: 'OK',
+          cancelButtonText: 'Cancel',
+          type: 'success'
+        }).then(() => {
+          this.request_order()
+          this.$message({
+            type: 'success',
+            message: 'Order request completed'
+          });
+        }).catch(() => {
+          this.$message({
+            type: 'info',
+            message: 'Order request canceled'
+          });          
+        });
+    },
+    request_order: function() {
+
+      console.log("request_order")
+      console.log("this.shop", this.shop)
+      // Must be truly random & 81-trytes long.
+      const seed = generateSeed();
+
+      console.log("products", this.products)
+
+      let request_object = {
+        name: 'nile shopping application',
+        products: this.products
+      }
+
+      const message = Converter.asciiToTrytes(JSON.stringify(request_object))
+
+
+      // Array of transfers which defines transfer recipients and value transferred in IOTAs.
+      const transfers = [{
+          address: this.shop.order_request_address,
+          value: 0, // 1Ki
+          tag: '', // optional tag of `0-27` trytes
+          message: message // optional message in trytes
+      }]
+      console.log("transfers", transfers)
+
+      // Depth or how far to go for tip selection entry point.
+      const depth = 3 
+
+      // Difficulty of Proof-of-Work required to attach transaction to tangle.
+      // Minimum value on mainnet is `14`, `7` on spamnet and `9` on devnet and other testnets.
+      const minWeightMagnitude = 14
+
+      // Prepare a bundle and signs it.
+      iota.prepareTransfers(seed, transfers)
+          .then(trytes => {
+              // Persist trytes locally before sending to network.
+              // This allows for reattachments and prevents key reuse if trytes can't
+              // be recovered by querying the network after broadcasting.
+
+              // Does tip selection, attaches to tangle by doing PoW and broadcasts.
+              return iota.sendTrytes(trytes, depth, minWeightMagnitude)
+          })
+          .then(bundle => {
+              console.log(`Published transaction with tail hash: ${bundle[0].hash}`)
+              console.log(`Bundle: ${bundle}`)
+          })
+          .catch(err => {
+              // handle errors here
+          })
     }
   }
 };
