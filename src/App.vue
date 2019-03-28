3709279a9165b855f898d484f87b127db02c4c12
huhn <sehe89@gmail.com>
2019-03-28 10:13:29 +0100

    remove text center

--- src/App.vue
+++ src/App.vue
@@ -39,10 +39,7 @@ body {
   font-family: 'Nunito', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-
-  
+ 
 }
 
 h1, h2, h3, h4 {
